5b6c3c28d0e2856e3653b8a4134fd428246313ad
Sude Gül ÜZÜM <128133604+sudeguluzum@users.noreply.github.com>
2024-07-14 22:08:19 +0300

    Create carpma_bolme_us_fonksiyonlari.py

---
+++ carpma_bolme_us_fonksiyonlari.py
@@ -0,0 +1,24 @@
+#ÇARPMA FONKSİYONU 
+def carpma(a,b):
+    return a*b
+carpim=carpma(3,5)
+print(carpim)
+
+
+#BÖLME FONKSİYONU
+def bolme(a,b):
+    return a/b
+bolme=bolme(6,2)
+print(int(bolme))
+
+
+# BİR DİĞER BÖLME FONKSİYONU
+def bolme(a,b):
+    return a/b
+print(bolme(4,2))
+
+
+#ÜS ALMA FONKSYİONU
+def us(a,b):
+    return a**b
+print(us(2,2))
