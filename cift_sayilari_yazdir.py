cbe234d0f1e5ec5b01263654fcd74be1444bc34f
Sude Gül ÜZÜM <128133604+sudeguluzum@users.noreply.github.com>
2024-07-14 22:02:36 +0300

    Update cift_sayilari_yazdir.py

--- cift_sayilari_yazdir.py
+++ cift_sayilari_yazdir.py
@@ -5,3 +5,9 @@ k=1
 for i in range(1,sayi+1):
     if(i%2==0):
         print(i)
+
+
+#0'dan başlayıp girilen sayıya kadar 2'şerli artsın
+sayi=int(input("Bir sayı giriniz: "))
+for i in range(0,sayi+1,2):
+    print(i)
