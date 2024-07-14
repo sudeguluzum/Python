e4e7c27ae88d015b1f0535bc6635c47983fc9686
Sude Gül ÜZÜM <128133604+sudeguluzum@users.noreply.github.com>
2024-07-14 22:05:16 +0300

    Create us_hesaplama.py

---
+++ us_hesaplama.py
@@ -0,0 +1,8 @@
+#Girilen sayıların üssünü alma
+def us(a,b):
+    return a**b # a sayısının b üssünü alma
+    
+a=int(input("Birinci Sayıyı Giriniz: "))
+b=int(input("İkinci Sayıyı Giriniz: "))
+deger = us(a,b)
+print(deger)
