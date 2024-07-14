0b98514f53fa2c49c36878088cf97d15c7015cc6
Sude Gül ÜZÜM <128133604+sudeguluzum@users.noreply.github.com>
2024-07-14 21:58:49 +0300

    Create 1to100.py

---
+++ 1to100.py
@@ -0,0 +1,10 @@
+# while döngüsü ile 1'den 100'e kadar birer birer artırsın
+toplam=100
+i=1
+while toplam>=i:
+    print(i)
+    i=i+1
+
+#for döngüsü ile ile 1'den 100'e kadar birer birer artırsın    
+for i in range(1,101):
+    print(i)
