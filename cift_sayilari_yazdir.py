9a8e18d73e7b4aa6450b8a447f2e6d834378dc3e
Sude Gül ÜZÜM <128133604+sudeguluzum@users.noreply.github.com>
2024-07-14 22:14:24 +0300

    Update cift_sayilari_yazdir.py

--- cift_sayilari_yazdir.py
+++ cift_sayilari_yazdir.py
@@ -11,3 +11,17 @@ for i in range(1,sayi+1):
 sayi=int(input("Bir sayı giriniz: "))
 for i in range(0,sayi+1,2):
     print(i)
+
+
+
+#Girilen sayıya kadar olan çift olanlarını ekrana yazdırma
+def fonk(sayi):    #fonk adında bir fonksiyon tanımla ve içine sayi değişkenini at
+    toplam=0     #toplamı 0'a eşitle
+    for i in range(1,sayi+1):
+        if i%2==0:    #sayi çift ise
+            print(i)      #çift sayıları ekrana yazdır
+            toplam=toplam+1     #toplamı 1 arttır
+    return toplam    #toplamı döndür
+sayi=int(input("Bir sayı giriniz:"))
+print(fonk(sayi)) 
+
